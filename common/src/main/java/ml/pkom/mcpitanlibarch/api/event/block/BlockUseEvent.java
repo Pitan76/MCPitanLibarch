9333e51d7d2089763f2edcbe89a61d4bd6a94ac8
PTOM76 <ptms76@outlook.jp>
2022-09-28 23:28:30 +0900

    add API

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/event/block/BlockUseEvent.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/event/block/BlockUseEvent.java
@@ -1,6 +1,7 @@
-package ml.pkom.mcpitanlibarch.api.event;
+package ml.pkom.mcpitanlibarch.api.event.block;
 
 import ml.pkom.mcpitanlibarch.api.entity.Player;
+import ml.pkom.mcpitanlibarch.api.event.BaseEvent;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Hand;
@@ -8,7 +9,7 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class BlockUseEvent {
+public class BlockUseEvent extends BaseEvent {
     public BlockState state;
     public World world;
     public BlockPos pos;
