08aff7fa0437d8feeebfb0a4ce328c9dfb8a8b66
PTOM76 <ptms76@outlook.jp>
2022-10-01 11:31:05 +0900

    Add API Add Example

--- common/src/main/java/ml/pkom/mcpitanlibarch/MCPitanLibarch.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/MCPitanLibarch.java
@@ -1,9 +1,14 @@
 package ml.pkom.mcpitanlibarch;
 
+import ml.pkom.mcpitanlibarch.api.util.PlatformUtil;
+import ml.pkom.mcpitanlibarch.test.ExampleMod;
+
 public class MCPitanLibarch {
     public static final String MOD_ID = "mcpitanlibarch";
 
     public static void init() {
-
+        if (PlatformUtil.isDevelopmentEnvironment()) {
+            ExampleMod.init();
+        }
     }
 }
---
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/util/PlatformUtil.java
@@ -0,0 +1,54 @@
+package ml.pkom.mcpitanlibarch.api.util;
+
+import me.shedaniel.architectury.platform.Platform;
+import me.shedaniel.architectury.utils.Env;
+import net.fabricmc.api.EnvType;
+
+import java.nio.file.Path;
+import java.util.Collection;
+
+public class PlatformUtil {
+    public static boolean isDevelopmentEnvironment() {
+        return Platform.isDevelopmentEnvironment();
+    }
+
+    public static boolean isFabric() {
+        return Platform.isFabric();
+    }
+
+    public static boolean isForge() {
+        return Platform.isForge();
+    }
+
+    public static boolean isModLoaded(String id) {
+        return Platform.isModLoaded(id);
+    }
+
+    public static boolean isClient() {
+        return Platform.getEnvironment() == Env.CLIENT;
+    }
+
+    public static boolean isServer() {
+        return Platform.getEnvironment() == Env.SERVER;
+    }
+
+    public static Path getConfigFolder() {
+        return Platform.getConfigFolder();
+    }
+
+    public static Path getGameFolder() {
+        return Platform.getGameFolder();
+    }
+
+    public static Path getModsFolder() {
+        return Platform.getModsFolder();
+    }
+
+    public static Collection<String> getModIds() {
+        return Platform.getModIds();
+    }
+
+    public static EnvType getEnv() {
+        return Platform.getEnv();
+    }
+}
