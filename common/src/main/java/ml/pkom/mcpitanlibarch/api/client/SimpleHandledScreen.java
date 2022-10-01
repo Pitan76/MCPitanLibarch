b44995e3a7262ab80aa9c06834aa9300960392e0
PTOM76 <ptms76@outlook.jp>
2022-10-01 15:28:10 +0900

    compatibility

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/client/SimpleHandledScreen.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/client/SimpleHandledScreen.java
@@ -2,6 +2,8 @@ package ml.pkom.mcpitanlibarch.api.client;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.entity.player.PlayerInventory;
@@ -14,7 +16,10 @@ public abstract class SimpleHandledScreen extends HandledScreen<ScreenHandler> {
         super(handler, inventory, title);
     }
 
-    protected <T extends ClickableWidget> T addDrawableChild_compatibility(T drawableElement) {
-        return super.addButton(drawableElement);
+    protected <T extends Element & Drawable> T addDrawableChild_compatibility(T drawableElement) {
+        if (drawableElement instanceof ClickableWidget)
+            return (T) super.addButton((ClickableWidget) drawableElement);
+        else
+            return super.addChild(drawableElement);
     }
 }
