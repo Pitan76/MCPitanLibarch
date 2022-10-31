efd6eff40cdc010d5a0d6d88c5d52931e12e63f5
PTOM76 <ptms76@outlook.jp>
2022-11-01 00:01:39 +0900

    1.0.10

--- common/src/main/java/ml/pkom/mcpitanlibarch/test/ExampleScreenHandler.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/test/ExampleScreenHandler.java
@@ -2,6 +2,7 @@ package ml.pkom.mcpitanlibarch.test;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 
 public class ExampleScreenHandler extends ScreenHandler {
@@ -9,6 +10,11 @@ public class ExampleScreenHandler extends ScreenHandler {
     public ExampleScreenHandler(int i, PlayerInventory playerInventory) {
         super(ExampleMod.supplierEXAMPLE_SCREENHANDLER.getOrNull(), i);
 
+    }
+
+    @Override
+    public ItemStack transferSlot(PlayerEntity player, int index) {
+        return null;
     }
 
     @Override
