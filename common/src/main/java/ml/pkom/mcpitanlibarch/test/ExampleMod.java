e82f6cc92d4866c9f9ca894b4a2769ef752ab006
PTOM76 <ptms76@outlook.jp>
2022-10-31 23:56:35 +0900

    1.0.10

--- common/src/main/java/ml/pkom/mcpitanlibarch/test/ExampleMod.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/test/ExampleMod.java
@@ -8,6 +8,7 @@ import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -28,10 +29,22 @@ public class ExampleMod {
 
     public static ArchRegistry registry = ArchRegistry.createRegistry(MOD_ID);
 
+    public static RegistryEvent<ScreenHandlerType<?>> supplierEXAMPLE_SCREENHANDLER = registry.registerScreenHandlerType(id("example_gui"), () -> new ScreenHandlerType<>(ExampleScreenHandler::new));;
+
     public static void init() {
         registry.registerItem(id("example_item"), () -> new Item(new Item.Settings().group(ItemGroup.MISC)));
         RegistryEvent<Block> EXAMPLE_BLOCK_SUPPLIER = registry.registerBlock(id("example_block"), () -> new Block(AbstractBlock.Settings.of(Material.STONE)));
         registry.registerItem(id("example_block"), () -> new BlockItem(EXAMPLE_BLOCK_SUPPLIER.supplier.get(), new Item.Settings().group(ItemGroup.MISC)));
+
+
+
+        registry.registerItem(id("example_gui_item"), () -> new ExampleGuiItem(new Item.Settings().group(ItemGroup.MISC)));
+
+
+
+        RegistryEvent<Block> EXAMPLE_GUI_BLOCK_SUPPLIER = registry.registerBlock(id("example_gui_block"), () -> new ExampleGuiBlock(AbstractBlock.Settings.of(Material.STONE)));
+        registry.registerItem(id("example_gui_block"), () -> new BlockItem(EXAMPLE_GUI_BLOCK_SUPPLIER.supplier.get(), new Item.Settings().group(ItemGroup.MISC)));
+
         registry.allRegister();
     }
 }
