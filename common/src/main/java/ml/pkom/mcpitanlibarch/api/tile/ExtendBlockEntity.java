75fca4e649216afed3d00cecbd0ff3a6292c3e60
PTOM76 <ptms76@outlook.jp>
2022-11-20 01:55:30 +0900

    1.1.8

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/tile/ExtendBlockEntity.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/tile/ExtendBlockEntity.java
@@ -1,17 +1,26 @@
 package ml.pkom.mcpitanlibarch.api.tile;
 
 import ml.pkom.mcpitanlibarch.api.event.block.TileCreateEvent;
+import ml.pkom.mcpitanlibarch.api.world.ExtendWorld;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class ExtendBlockEntity extends BlockEntity {
+    ExtendWorld world;
     public ExtendBlockEntity(BlockEntityType<?> type) {
         super(type);
     }
 
     public ExtendBlockEntity(BlockEntityType<?> type, TileCreateEvent event) {
-        this(type);
+        this(type, event.getBlockPos(), event.getBlockState());
+    }
+
+    @Override
+    public void setWorld(World world) {
+        super.setWorld(world);
+        this.world = new ExtendWorld(world);
     }
 }
