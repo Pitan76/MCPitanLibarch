9d7fc6a6db629d4a49de67cf14b7ea2894e56808
PTOM76 <ptms76@outlook.jp>
2022-12-08 14:33:45 +0900

    a

---
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/item/DefaultItemGroups.java
@@ -0,0 +1,21 @@
+package ml.pkom.mcpitanlibarch.api.item;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+
+public class DefaultItemGroups {
+    public static final ItemGroup BUILDING_BLOCKS = ItemGroups.getGroups().get(0);
+    public static final ItemGroup COLORED_BLOCKS = ItemGroups.getGroups().get(1);
+    public static final ItemGroup NATURAL = ItemGroups.getGroups().get(2);
+    public static final ItemGroup FUNCTIONAL = ItemGroups.getGroups().get(3);
+    public static final ItemGroup REDSTONE = ItemGroups.getGroups().get(4);
+    public static final ItemGroup HOTBAR = ItemGroups.getGroups().get(5);
+    public static final ItemGroup SEARCH = ItemGroups.getGroups().get(6);
+    public static final ItemGroup TOOLS = ItemGroups.getGroups().get(7);
+    public static final ItemGroup COMBAT = ItemGroups.getGroups().get(8);
+    public static final ItemGroup FOOD_AND_DRINK = ItemGroups.getGroups().get(9);
+    public static final ItemGroup INGREDIENTS = ItemGroups.getGroups().get(10);
+    public static final ItemGroup SPAWN_EGGS = ItemGroups.getGroups().get(11);
+    public static final ItemGroup OPERATOR = ItemGroups.getGroups().get(12);
+    public static final ItemGroup INVENTORY = ItemGroups.getGroups().get(13);
+}
--- common/src/main/java/ml/pkom/mcpitanlibarch/test/ExampleScreenHandler.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/test/ExampleScreenHandler.java
@@ -1,14 +1,20 @@
 package ml.pkom.mcpitanlibarch.test;
 
+import ml.pkom.mcpitanlibarch.api.gui.SimpleScreenHandler;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.screen.ScreenHandler;
+import net.minecraft.item.ItemStack;
 
-public class ExampleScreenHandler extends ScreenHandler {
+public class ExampleScreenHandler extends SimpleScreenHandler {
 
     public ExampleScreenHandler(int i, PlayerInventory playerInventory) {
         super(ExampleMod.supplierEXAMPLE_SCREENHANDLER.getOrNull(), i);
 
+    }
+
+    @Override
+    public ItemStack quickMoveOverride(PlayerEntity player, int slot) {
+        return null;
     }
 
     @Override
