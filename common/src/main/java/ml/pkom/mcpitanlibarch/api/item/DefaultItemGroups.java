76041454e8116d4221bcb50bb1469588dd40ab92
PTOM76 <ptms76@outlook.jp>
2022-12-08 14:33:45 +0900

    a

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/item/DefaultItemGroups.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/item/DefaultItemGroups.java
@@ -1,21 +1,20 @@
 package ml.pkom.mcpitanlibarch.api.item;
 
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
 
 public class DefaultItemGroups {
-    public static final ItemGroup BUILDING_BLOCKS = ItemGroups.getGroups().get(0);
-    public static final ItemGroup COLORED_BLOCKS = ItemGroups.getGroups().get(1);
-    public static final ItemGroup NATURAL = ItemGroups.getGroups().get(2);
-    public static final ItemGroup FUNCTIONAL = ItemGroups.getGroups().get(3);
-    public static final ItemGroup REDSTONE = ItemGroups.getGroups().get(4);
-    public static final ItemGroup HOTBAR = ItemGroups.getGroups().get(5);
-    public static final ItemGroup SEARCH = ItemGroups.getGroups().get(6);
-    public static final ItemGroup TOOLS = ItemGroups.getGroups().get(7);
-    public static final ItemGroup COMBAT = ItemGroups.getGroups().get(8);
-    public static final ItemGroup FOOD_AND_DRINK = ItemGroups.getGroups().get(9);
-    public static final ItemGroup INGREDIENTS = ItemGroups.getGroups().get(10);
-    public static final ItemGroup SPAWN_EGGS = ItemGroups.getGroups().get(11);
-    public static final ItemGroup OPERATOR = ItemGroups.getGroups().get(12);
-    public static final ItemGroup INVENTORY = ItemGroups.getGroups().get(13);
+    public static final ItemGroup BUILDING_BLOCKS = ItemGroup.BUILDING_BLOCKS;
+    public static final ItemGroup COLORED_BLOCKS = ItemGroup.BUILDING_BLOCKS;
+    public static final ItemGroup NATURAL = ItemGroup.BUILDING_BLOCKS;
+    public static final ItemGroup FUNCTIONAL = ItemGroup.DECORATIONS;
+    public static final ItemGroup REDSTONE = ItemGroup.REDSTONE;
+    public static final ItemGroup HOTBAR = ItemGroup.HOTBAR;
+    public static final ItemGroup SEARCH = ItemGroup.SEARCH;
+    public static final ItemGroup TOOLS = ItemGroup.TOOLS;
+    public static final ItemGroup COMBAT = ItemGroup.COMBAT;
+    public static final ItemGroup FOOD_AND_DRINK = ItemGroup.FOOD;
+    public static final ItemGroup INGREDIENTS = ItemGroup.MISC;
+    public static final ItemGroup SPAWN_EGGS = ItemGroup.MISC;
+    public static final ItemGroup OPERATOR = ItemGroup.MISC;
+    public static final ItemGroup INVENTORY = ItemGroup.INVENTORY;
 }
