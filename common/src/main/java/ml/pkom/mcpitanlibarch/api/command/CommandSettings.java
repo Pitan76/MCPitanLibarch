bc34d1f42aa73e922ad1046f0da6826cc839cd68
PTOM76 <ptms76@outlook.jp>
2022-12-13 16:56:53 +0900

    Change Command API 1.19

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/command/CommandSettings.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/command/CommandSettings.java
@@ -1,4 +1,33 @@
 package ml.pkom.mcpitanlibarch.api.command;
 
+import net.minecraft.server.command.ServerCommandSource;
+
 public class CommandSettings {
+    private int permissionLevel = -1;
+    private ICustom iCustom = null;
+
+    public boolean requires(ServerCommandSource source) {
+
+        return customRequires(source) && (permissionLevel == -1 || source.hasPermissionLevel(permissionLevel));
+    }
+
+    private boolean customRequires(ServerCommandSource source) {
+        if (iCustom == null) return true;
+        return iCustom.custom(source);
+    }
+
+    public CommandSettings permissionLevel(int level) {
+        this.permissionLevel = level;
+        return this;
+    }
+
+    public CommandSettings custom(ICustom iCustom) {
+        this.iCustom = iCustom;
+        return this;
+    }
+
+    @FunctionalInterface
+    public interface ICustom {
+        boolean custom(ServerCommandSource source);
+    }
 }
