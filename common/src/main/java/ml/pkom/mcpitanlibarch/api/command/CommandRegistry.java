efd21435f8a254223a7ef2aeb830cee5d167f34d
PTOM76 <ptms76@outlook.jp>
2022-12-13 17:56:27 +0900

    fix

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/command/CommandRegistry.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/command/CommandRegistry.java
@@ -35,7 +35,7 @@ public class CommandRegistry {
         );
     }
 
-    private static void forArgsCmd(AbstractCommand<?> absCmd, ArgumentBuilder<? extends ServerCommandSource, ?> builder) {
+    private static <T extends ArgumentBuilder<ServerCommandSource, T>> void forArgsCmd(AbstractCommand<?> absCmd, ArgumentBuilder<ServerCommandSource, T> builder) {
 
         if (!absCmd.getArgumentCommands().isEmpty()) {
             // 引数コマンド
@@ -92,7 +92,7 @@ public class CommandRegistry {
                             );
                 }
                 forArgsCmd(argCmd.getValue(), nextBuilder);
-                ((LiteralArgumentBuilder<ServerCommandSource>) builder).then(nextBuilder);
+                builder.then(nextBuilder);
             }
         }
 
