b21d381d5fe4c24e3b3a263b9c9ecd1ef8495420
PTOM76 <ptms76@outlook.jp>
2022-12-23 15:05:06 +0900

    fix item group

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/item/DefaultItemGroups.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/item/DefaultItemGroups.java
@@ -3,24 +3,68 @@ package ml.pkom.mcpitanlibarch.api.item;
 import net.minecraft.item.ItemGroup;
 
 public class DefaultItemGroups {
-    public static final ItemGroup BUILDING_BLOCKS = ItemGroup.BUILDING_BLOCKS;
-    public static final ItemGroup COLORED_BLOCKS = ItemGroup.BUILDING_BLOCKS;
-    public static final ItemGroup NATURAL = ItemGroup.DECORATIONS;
-    public static final ItemGroup FUNCTIONAL = ItemGroup.TRANSPORTATION;
-    public static final ItemGroup REDSTONE = ItemGroup.REDSTONE;
-    public static final ItemGroup HOTBAR = ItemGroup.HOTBAR;
-    public static final ItemGroup SEARCH = ItemGroup.SEARCH;
-    public static final ItemGroup TOOLS = ItemGroup.TOOLS;
-    public static final ItemGroup COMBAT = ItemGroup.COMBAT;
-    public static final ItemGroup FOOD_AND_DRINK = ItemGroup.FOOD;
-    public static final ItemGroup INGREDIENTS = ItemGroup.MISC;
-    public static final ItemGroup SPAWN_EGGS = ItemGroup.MISC;
-    public static final ItemGroup OPERATOR = ItemGroup.MISC;
-    public static final ItemGroup INVENTORY = ItemGroup.INVENTORY;
+
+    public static final ItemGroup BUILDING_BLOCKS;
+    public static final ItemGroup COLORED_BLOCKS;
+    public static final ItemGroup NATURAL;
+    public static final ItemGroup FUNCTIONAL;
+    public static final ItemGroup REDSTONE;
+    public static final ItemGroup HOTBAR;
+    public static final ItemGroup SEARCH;
+    public static final ItemGroup TOOLS;
+    public static final ItemGroup COMBAT;
+    public static final ItemGroup FOOD_AND_DRINK;
+    public static final ItemGroup INGREDIENTS;
+    public static final ItemGroup SPAWN_EGGS;
+    public static final ItemGroup OPERATOR;
+    public static final ItemGroup INVENTORY;
 
     // ～1.19.2 Item Group
-    public static final ItemGroup BREWING = ItemGroup.BREWING; // if version is 1.19.3, FOOD_AND_DRINK
-    public static final ItemGroup TRANSPORTATION = ItemGroup.TRANSPORTATION; // if version is 1.19.3, FUNCTIONAL
-    public static final ItemGroup DECORATIONS = ItemGroup.DECORATIONS; // if version is 1.19.3, NATURAL
-    public static final ItemGroup MISC = ItemGroup.MISC; // if version is 1.19.3, INGREDIENTS
+    public static final ItemGroup BREWING; // if version is 1.19.3, FOOD_AND_DRINK
+    public static final ItemGroup TRANSPORTATION; // if version is 1.19.3, FUNCTIONAL
+    public static final ItemGroup DECORATIONS; // if version is 1.19.3, NATURAL
+    public static final ItemGroup MISC; // if version is 1.19.3, INGREDIENTS
+    
+    static {
+        BUILDING_BLOCKS = ItemGroup.BUILDING_BLOCKS;
+        COLORED_BLOCKS = ItemGroup.BUILDING_BLOCKS;
+        NATURAL = ItemGroup.DECORATIONS;
+        FUNCTIONAL = ItemGroup.TRANSPORTATION;
+        REDSTONE = ItemGroup.REDSTONE;
+
+        ItemGroup HOTBAR_TMP;
+        try {
+            HOTBAR_TMP = ItemGroup.HOTBAR;
+        } catch (Exception ignored) {
+            HOTBAR_TMP = null;
+        }
+
+        HOTBAR = HOTBAR_TMP;
+
+        ItemGroup SEARCH_TMP;
+        try {
+            SEARCH_TMP = ItemGroup.SEARCH;
+        } catch (Exception ignored) {
+            SEARCH_TMP = null;
+        }
+        SEARCH = SEARCH_TMP;
+        TOOLS = ItemGroup.TOOLS;
+        COMBAT = ItemGroup.COMBAT;
+        FOOD_AND_DRINK = ItemGroup.FOOD;
+        INGREDIENTS = ItemGroup.MISC;
+        SPAWN_EGGS = ItemGroup.MISC;
+        OPERATOR = ItemGroup.MISC;
+
+        ItemGroup INVENTORY_TMP;
+        try {
+            INVENTORY_TMP = ItemGroup.INVENTORY;
+        } catch (Exception ignored) {
+            INVENTORY_TMP = null;
+        }
+        INVENTORY = INVENTORY_TMP;
+        BREWING = ItemGroup.BREWING;
+        TRANSPORTATION = ItemGroup.TRANSPORTATION; 
+        DECORATIONS = ItemGroup.DECORATIONS; 
+        MISC = ItemGroup.MISC; 
+    }
 }
