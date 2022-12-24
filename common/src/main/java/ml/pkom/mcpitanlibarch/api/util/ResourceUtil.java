ee4fbaa92fcb84c1d8190da44261eb9c9302de3e
PTOM76 <ptms76@outlook.jp>
2022-12-24 23:53:16 +0900

    add api

---
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/util/ResourceUtil.java
@@ -0,0 +1,27 @@
+package ml.pkom.mcpitanlibarch.api.util;
+
+import net.minecraft.resource.Resource;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResourceUtil {
+    public static Resource getResource(ResourceManager resourceManager, Identifier identifier) throws IOException {
+        return resourceManager.getResource(identifier);
+    }
+
+    public static Map<Identifier, Resource> findResources(ResourceManager resourceManager, String startingPath, String endingPath) throws IOException {
+        Map<Identifier, Resource> map = new HashMap<>();
+        for (Identifier identifier : resourceManager.findResources(startingPath, s -> s.endsWith(endingPath))) {
+            map.put(identifier, resourceManager.getResource(identifier));
+        }
+        return map;
+    }
+
+    public static void close(Resource resource) throws IOException {
+        resource.close();
+    }
+}
