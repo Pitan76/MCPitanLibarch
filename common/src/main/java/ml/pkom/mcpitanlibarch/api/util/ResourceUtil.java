3260abe5a5206e9e56960852f86717d6ed3ba541
PTOM76 <ptms76@outlook.jp>
2022-12-25 00:38:57 +0900

    1.19.2

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/util/ResourceUtil.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/util/ResourceUtil.java
@@ -5,6 +5,7 @@ import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 public class ResourceUtil {
@@ -12,11 +13,15 @@ public class ResourceUtil {
         return resourceManager.getResource(identifier).get();
     }
 
-    public static Map<Identifier, Resource> findResources(ResourceManager resourceManager, String startingPath, String endingPath) {
+    public static InputStream getInputStream(Resource resource) throws IOException {
+        return resource.getInputStream();
+    }
+
+    public static Map<Identifier, Resource> findResources(ResourceManager resourceManager, String startingPath, String endingPath) throws IOException {
         return resourceManager.findResources(startingPath, s -> s.toString().endsWith(endingPath));
     }
 
     public static void close(Resource resource) throws IOException {
-        resource.getInputStream().close();
+        getInputStream(resource);
     }
 }
