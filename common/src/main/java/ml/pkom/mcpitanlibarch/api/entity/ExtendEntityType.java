5c4693ed37cc7a68d89ddb0901259708ebabef28
PTOM76 <ptms76@outlook.jp>
2023-02-10 00:48:29 +0900

    1.19.3

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/entity/ExtendEntityType.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/entity/ExtendEntityType.java
@@ -6,12 +6,13 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.resource.featuretoggle.FeatureFlags;
 
 public class ExtendEntityType<T extends Entity> extends EntityType<T> {
     private final Boolean alwaysUpdateVelocity;
 
     public ExtendEntityType(EntityType.EntityFactory<T> factory, SpawnGroup spawnGroup, boolean saveable, boolean summonable, boolean fireImmune, boolean spawnableFarFromPlayer, ImmutableSet<Block> canSpawnBlocks, EntityDimensions entityDimensions, int maxTrackDistance, int trackTickInterval, Boolean alwaysUpdateVelocity) {
-        super(factory, spawnGroup, saveable, summonable, fireImmune, spawnableFarFromPlayer, canSpawnBlocks, entityDimensions, maxTrackDistance, trackTickInterval);
+        super(factory, spawnGroup, saveable, summonable, fireImmune, spawnableFarFromPlayer, canSpawnBlocks, entityDimensions, maxTrackDistance, trackTickInterval, FeatureFlags.DEFAULT_ENABLED_FEATURES);
         this.alwaysUpdateVelocity = alwaysUpdateVelocity;
     }
 
