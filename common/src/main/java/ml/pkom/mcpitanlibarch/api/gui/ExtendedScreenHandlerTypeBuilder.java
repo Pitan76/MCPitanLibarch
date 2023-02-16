e550e4b5aaf5b06f06db849c97047ac3a3959fef
PTOM76 <ptms76@outlook.jp>
2023-02-16 16:33:04 +0900

    fix extendScreen

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/gui/ExtendedScreenHandlerTypeBuilder.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/gui/ExtendedScreenHandlerTypeBuilder.java
@@ -1,21 +1,21 @@
 package ml.pkom.mcpitanlibarch.api.gui;
 
+import me.shedaniel.architectury.registry.MenuRegistry;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
 
-public class ExtendedScreenHandlerType<T extends ExtendedScreenHandler> extends ScreenHandlerType<T> {
+public class ExtendedScreenHandlerTypeBuilder<T extends ExtendedScreenHandler> {
 
     private final Factory<T> factory;
 
-    public ExtendedScreenHandlerType(Factory<T> factory) {
-        super(null);
+    public ExtendedScreenHandlerTypeBuilder(Factory<T> factory) {
         this.factory = factory;
     }
 
-    public T create(int syncId, PlayerInventory inventory, PacketByteBuf buf) {
-        return factory.create(syncId, inventory, buf);
+    public ScreenHandlerType<T> build() {
+        return MenuRegistry.ofExtended(factory::create);
     }
 
     @FunctionalInterface
