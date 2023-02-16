f996618680775b896e7c0e908595f9fff574570e
PTOM76 <ptms76@outlook.jp>
2023-02-16 15:45:33 +0900

    add ArchRegistryClient

---
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/client/registry/ArchRegistryClient.java
@@ -0,0 +1,19 @@
+package ml.pkom.mcpitanlibarch.api.client.registry;
+
+import dev.architectury.registry.menu.MenuRegistry;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.ScreenHandlerProvider;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.text.Text;
+
+public class ArchRegistryClient {
+    public <H extends ScreenHandler, S extends Screen & ScreenHandlerProvider<H>> void registerScreen(ScreenHandlerType<? extends H> type, ScreenFactory<H, S> factory) {
+        MenuRegistry.registerScreenFactory(type, factory::create);
+    }
+
+    public interface ScreenFactory<H extends ScreenHandler, S extends Screen & ScreenHandlerProvider<H>> {
+        S create(H handler, PlayerInventory inventory, Text text);
+    }
+}
