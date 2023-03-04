7895fc2801513451199722f7fdca073e8da2b468
PTOM76 <ptms76@outlook.jp>
2023-03-04 22:30:48 +0900

    ScreenUtil api

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/util/client/ScreenUtil.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/util/client/ScreenUtil.java
@@ -1,12 +1,11 @@
 package ml.pkom.mcpitanlibarch.api.util.client;
 
-import com.mojang.blaze3d.systems.RenderSystem;
+import ml.pkom.mcpitanlibarch.api.client.gui.widget.RedrawableTexturedButtonWidget;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.gui.widget.TexturedButtonWidget;
+import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
@@ -15,9 +14,9 @@ import javax.annotation.Nullable;
 @Environment(EnvType.CLIENT)
 public class ScreenUtil {
     public static void setBackground(Identifier GUI, float f, float g, float h, float i) {
-        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
-        RenderSystem.setShaderColor(f, g, h, i);
-        RenderSystem.setShaderTexture(0, GUI);
+        RenderUtil.setShaderToPositionTexProgram();
+        RenderUtil.setShaderColor(f, g, h, i);
+        RenderUtil.setShaderTexture(0, GUI);
 
         // GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
         // MinecraftClient.getInstance().getTextureManager().bindTexture(GUI);
@@ -41,5 +40,45 @@ public class ScreenUtil {
             builder.narrationSupplier(tooltipSupplier);
 
         return builder.build();
+    }
+
+    public static TexturedButtonWidget createTexturedButtonWidget(int x, int y, int width, int height, int u, int v, Identifier texture, ButtonWidget.PressAction pressAction) {
+        return createTexturedButtonWidget(x, y, width, height, u, v, height, texture, pressAction);
+    }
+
+    public static TexturedButtonWidget createTexturedButtonWidget(int x, int y, int width, int height, int u, int v, int hoveredVOffset, Identifier texture, ButtonWidget.PressAction pressAction) {
+        return createTexturedButtonWidget(x, y, width, height, u, v, hoveredVOffset, texture, 256, 256, pressAction);
+    }
+
+    public static TexturedButtonWidget createTexturedButtonWidget(int x, int y, int width, int height, int u, int v, int hoveredVOffset, Identifier texture, int textureWidth, int textureHeight, ButtonWidget.PressAction pressAction) {
+        return createTexturedButtonWidget(x, y, width, height, u, v, hoveredVOffset, texture, textureWidth, textureHeight, pressAction, Texts.empty());
+    }
+
+    public static TexturedButtonWidget createTexturedButtonWidget(int x, int y, int width, int height, int u, int v, int hoveredVOffset, Identifier texture, int textureWidth, int textureHeight, ButtonWidget.PressAction pressAction, Text message) {
+        return new TexturedButtonWidget(x, y, width, height, u, v, hoveredVOffset, texture, textureWidth, textureHeight, pressAction, message);
+    }
+
+    public static RedrawableTexturedButtonWidget createRedrawableTexturedButtonWidget(int x, int y, int width, int height, int u, int v, Identifier texture, ButtonWidget.PressAction pressAction) {
+        return createRedrawableTexturedButtonWidget(x, y, width, height, u, v, height, texture, pressAction);
+    }
+
+    public static RedrawableTexturedButtonWidget createRedrawableTexturedButtonWidget(int x, int y, int width, int height, int u, int v, int hoveredVOffset, Identifier texture, ButtonWidget.PressAction pressAction) {
+        return createRedrawableTexturedButtonWidget(x, y, width, height, u, v, hoveredVOffset, texture, 256, 256, pressAction);
+    }
+
+    public static RedrawableTexturedButtonWidget createRedrawableTexturedButtonWidget(int x, int y, int width, int height, int u, int v, int hoveredVOffset, Identifier texture, int textureWidth, int textureHeight, ButtonWidget.PressAction pressAction) {
+        return createRedrawableTexturedButtonWidget(x, y, width, height, u, v, hoveredVOffset, texture, textureWidth, textureHeight, pressAction, Texts.empty());
+    }
+
+    public static RedrawableTexturedButtonWidget createRedrawableTexturedButtonWidget(int x, int y, int width, int height, int u, int v, int hoveredVOffset, Identifier texture, int textureWidth, int textureHeight, ButtonWidget.PressAction pressAction, Text message) {
+        return new RedrawableTexturedButtonWidget(x, y, width, height, u, v, hoveredVOffset, texture, textureWidth, textureHeight, pressAction, message);
+    }
+
+
+
+    public static class Texts {
+        public static Text empty() {
+            return ScreenTexts.EMPTY;
+        }
     }
 }
