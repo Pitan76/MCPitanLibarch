b5c3be68d7da1a3472ef1ae84d3e7e7ba530fe5a
PTOM76 <ptms76@outlook.jp>
2023-03-04 23:12:31 +0900

    fix api

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/util/client/RenderUtil.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/util/client/RenderUtil.java
@@ -1,20 +1,22 @@
 package ml.pkom.mcpitanlibarch.api.util.client;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.util.Identifier;
 
 public class RenderUtil {
     public static void setShaderToPositionTexProgram() {
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        //
     }
 
     public static void setShaderColor(float red, float green, float blue, float alpha) {
-        RenderSystem.setShaderColor(red, green, blue, alpha);
+        GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
     }
 
     public static void setShaderTexture(int texture, Identifier id) {
-        RenderSystem.setShaderTexture(texture, id);
+        MinecraftClient.getInstance().getTextureManager().bindTexture(id);
     }
 
     public static void enableDepthTest() {
