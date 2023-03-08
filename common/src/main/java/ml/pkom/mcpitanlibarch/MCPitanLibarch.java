dede1b41974969e3e0ad6c37380df8c8cba41153
PTOM76 <ptms76@outlook.jp>
2023-03-08 14:24:42 +0900

    add API

--- common/src/main/java/ml/pkom/mcpitanlibarch/MCPitanLibarch.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/MCPitanLibarch.java
@@ -3,7 +3,6 @@ package ml.pkom.mcpitanlibarch;
 import ml.pkom.easyapi.config.Config;
 import ml.pkom.easyapi.config.YamlConfig;
 import ml.pkom.mcpitanlibarch.api.util.PlatformUtil;
-import ml.pkom.mcpitanlibarch.test.ExampleMod;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -23,9 +22,11 @@ public class MCPitanLibarch {
 
     public static void init() {
         configInit();
+        /*
         if (PlatformUtil.isDevelopmentEnvironment()) {
             ExampleMod.init();
         }
+         */
     }
 
     public static void configInit() {
@@ -39,7 +40,8 @@ public class MCPitanLibarch {
             config.setString("item", "examplemod:hogehoge_item,examplemod:fuga_item");
             config.setString("block", "examplemod:hogehoge_block,examplemod:fuga_block");
 
-            config.load(configFile);
+            if (configFile.exists())
+                config.load(configFile);
 
             if (config.configMap.containsKey("item"))
                 itemBlackList.addAll(Arrays.asList(config.getString("item").split(",")));
