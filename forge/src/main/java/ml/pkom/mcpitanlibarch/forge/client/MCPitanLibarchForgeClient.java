8ab51e42743074e0b85bc1c6a2799bcbdf19528c
PTOM76 <ptms76@outlook.jp>
2023-03-14 12:43:28 +0900

    fix gradle etc

--- forge/src/main/java/ml/pkom/mcpitanlibarch/forge/client/MCPitanLibarchForgeClient.java
+++ forge/src/main/java/ml/pkom/mcpitanlibarch/forge/client/MCPitanLibarchForgeClient.java
@@ -1,12 +1,10 @@
 package ml.pkom.mcpitanlibarch.forge.client;
 
-import ml.pkom.mcpitanlibarch.api.util.PlatformUtil;
-import ml.pkom.mcpitanlibarch.test.ExampleModClient;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class MCPitanLibarchForgeClient {
     public static void clientInit(FMLClientSetupEvent event) {
-        if (PlatformUtil.isDevelopmentEnvironment())
-            ExampleModClient.init();
+        //if (PlatformUtil.isDevelopmentEnvironment())
+        //    ExampleModClient.init();
     }
 }
