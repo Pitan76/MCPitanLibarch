0545d8a83e06d17c333fa68ba5d4337660f6693f
PTOM76 <ptms76@outlook.jp>
2023-03-15 19:57:29 +0900

    fix

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/util/RecipeUtil.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/util/RecipeUtil.java
@@ -1,10 +1,14 @@
 package ml.pkom.mcpitanlibarch.api.util;
 
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.ShapelessRecipe;
+import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.world.World;
 
 public class RecipeUtil {
     public static ShapelessRecipe createShapelessRecipe(Identifier id, String group, CompatibilityCraftingRecipeCategory category, ItemStack output, DefaultedList<Ingredient> input) {
@@ -13,6 +17,15 @@ public class RecipeUtil {
 
     public static ShapelessRecipe createShapelessRecipe(Identifier id, String group, ItemStack output, DefaultedList<Ingredient> input) {
         return new ShapelessRecipe(id, group, output, input);
+    }
+
+    public static ItemStack craft(Recipe<Inventory> recipe, Inventory inventory, World world) {
+        return recipe.craft(inventory, world.getRegistryManager());
+    }
+
+
+    public static ItemStack getOutput(Recipe<Inventory> recipe, World world) {
+        return recipe.getOutput(world.getRegistryManager());
     }
 
     public enum CompatibilityCraftingRecipeCategory {
