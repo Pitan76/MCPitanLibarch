445abed6e8bc4ccade7373c9ef0a572342a9086f
PTOM76 <ptms76@outlook.jp>
2023-03-15 22:38:12 +0900

    Fix API

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/util/math/PosUtil.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/util/math/PosUtil.java
@@ -2,6 +2,7 @@ package ml.pkom.mcpitanlibarch.api.util.math;
 
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Position;
+import net.minecraft.util.math.Vec3d;
 
 public class PosUtil {
     public BlockPos flooredBlockPos(double x, double y, double z) {
@@ -9,6 +10,10 @@ public class PosUtil {
     }
 
     public BlockPos flooredBlockPos(Position pos) {
+        return new BlockPos(pos);
+    }
+
+    public static BlockPos flooredBlockPos(Vec3d pos) {
         return new BlockPos(pos);
     }
 }
