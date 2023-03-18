4a09b7697015f0f89904f4cadb00e532c947cedd
PTOM76 <ptms76@outlook.jp>
2023-03-18 17:44:27 +0900

    add APIs

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/item/ExtendSettings.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/item/ExtendSettings.java
@@ -1,13 +1,8 @@
 package ml.pkom.mcpitanlibarch.api.item;
 
-import dev.architectury.registry.CreativeTabRegistry;
-import ml.pkom.mcpitanlibarch.api.util.ItemUtil;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ExtendSettings extends Item.Settings {
 
