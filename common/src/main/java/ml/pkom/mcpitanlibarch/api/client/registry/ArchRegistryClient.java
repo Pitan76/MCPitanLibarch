184f9dfebb8ba79f62679adb3751d460f0f8a120
PTOM76 <ptms76@outlook.jp>
2023-03-22 14:10:09 +0900

    add API

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/client/registry/ArchRegistryClient.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/client/registry/ArchRegistryClient.java
@@ -1,12 +1,30 @@
 package ml.pkom.mcpitanlibarch.api.client.registry;
 
+import dev.architectury.event.events.client.ClientTextureStitchEvent;
+import dev.architectury.registry.client.level.entity.EntityRendererRegistry;
+import dev.architectury.registry.client.particle.ParticleProviderRegistry;
 import dev.architectury.registry.menu.MenuRegistry;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.ScreenHandlerProvider;
+import net.minecraft.client.particle.ParticleFactory;
+import net.minecraft.client.particle.SpriteProvider;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleType;
+import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.random.Random;
+
+import java.util.List;
+import java.util.function.Supplier;
 
 public class ArchRegistryClient {
     public static <H extends ScreenHandler, S extends Screen & ScreenHandlerProvider<H>> void registerScreen(ScreenHandlerType<? extends H> type, ScreenFactory<H, S> factory) {
@@ -15,5 +33,64 @@ public class ArchRegistryClient {
 
     public interface ScreenFactory<H extends ScreenHandler, S extends Screen & ScreenHandlerProvider<H>> {
         S create(H handler, PlayerInventory inventory, Text text);
+    }
+
+    public static <T extends ParticleEffect> void registerParticle(ParticleType<T> type, ParticleFactory<T> factory) {
+        ParticleProviderRegistry.register(type, factory);
+    }
+
+    public static <T extends ParticleEffect> void registerParticle(ParticleType<T> type, DeferredParticleProvider<T> provider) {
+        ParticleProviderRegistry.register(type, spriteSet -> provider.create(new ExtendedSpriteSet() {
+            @Override
+            public SpriteAtlasTexture getAtlas() {
+                return spriteSet.getAtlas();
+            }
+
+            @Override
+            public List<Sprite> getSprites() {
+                return spriteSet.getSprites();
+            }
+
+            @Override
+            public Sprite getSprite(int age, int maxAge) {
+                return spriteSet.getSprite(age, maxAge);
+            }
+
+            @Override
+            public Sprite getSprite(Random random) {
+                return spriteSet.getSprite(random);
+            }
+        }));
+    }
+
+    public static <T extends Entity> void registerEntityRenderer(Supplier<? extends EntityType<? extends T>> type, EntityRendererFactory<T> provider) {
+        EntityRendererRegistry.register(type, provider);
+    }
+
+    @FunctionalInterface
+    public interface DeferredParticleProvider<T extends ParticleEffect> {
+        ParticleFactory<T> create(ExtendedSpriteSet spriteSet);
+    }
+
+    public interface ExtendedSpriteSet extends SpriteProvider {
+        SpriteAtlasTexture getAtlas();
+
+        List<Sprite> getSprites();
+    }
+
+    public void registryClientSpriteAtlasTexture(Identifier identifier) {
+        registryClientSprite(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE, identifier);
+    }
+
+    public void registryClientSpriteAtlasTexture(Sprite sprite) {
+        registryClientSprite(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE, sprite);
+    }
+
+    public void registryClientSprite(Identifier atlasId, Identifier identifier) {
+        ClientTextureStitchEvent.PRE.register(((atlas, spriteAdder) -> spriteAdder.accept(identifier)));
+    }
+
+    public void registryClientSprite(Identifier atlasId, Sprite sprite) {
+        ClientTextureStitchEvent.PRE.register(((atlas, spriteAdder) -> spriteAdder.accept(sprite.getId())));
     }
 }
