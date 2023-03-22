eb9fd6cdf5405770091fe419a58db901a2545ef4
PTOM76 <ptms76@outlook.jp>
2023-03-22 14:10:09 +0900

    add API

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/util/ScreenHandlerUtil.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/util/ScreenHandlerUtil.java
@@ -1,13 +1,19 @@
 package ml.pkom.mcpitanlibarch.api.util;
 
+import dev.architectury.registry.menu.ExtendedMenuProvider;
+import dev.architectury.registry.menu.MenuRegistry;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.screen.slot.Slot;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.registry.Registry;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ScreenHandlerUtil {
     public static DefaultedList<Slot> getSlots(ScreenHandler screenHandler) {
@@ -24,5 +30,17 @@ public class ScreenHandlerUtil {
             screenHandlerTypes.add(screenHandler);
         }
         return screenHandlerTypes;
+    }
+
+    public static void openExtendedMenu(ServerPlayerEntity player, NamedScreenHandlerFactory provider, Consumer<PacketByteBuf> bufWriter) {
+        MenuRegistry.openExtendedMenu(player, provider, bufWriter);
+    }
+
+    public static void openExtendedMenu(ServerPlayerEntity player, ExtendedMenuProvider provider) {
+        MenuRegistry.openExtendedMenu(player, provider);
+    }
+
+    public static void openMenu(ServerPlayerEntity player, NamedScreenHandlerFactory provider) {
+        MenuRegistry.openMenu(player, provider);
     }
 }
