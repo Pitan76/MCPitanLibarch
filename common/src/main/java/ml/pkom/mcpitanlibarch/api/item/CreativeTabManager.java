c326e5ee62e8b3bdfa50d79f04c9a7343d578cb0
PTOM76 <ptms76@outlook.jp>
2023-03-31 14:08:13 +0900

    fix api ver

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/item/CreativeTabManager.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/item/CreativeTabManager.java
@@ -3,6 +3,7 @@ package ml.pkom.mcpitanlibarch.api.item;
 import dev.architectury.registry.CreativeTabRegistry;
 import ml.pkom.mcpitanlibarch.api.util.ItemUtil;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import java.util.List;
 
 public class CreativeTabManager {
     private static final List<BookingItem> bookingItems = new ArrayList<>();
+    private static final List<BookingStack> bookingStacks = new ArrayList<>();
 
     // グループ予約済みアイテム
     public static class BookingItem {
@@ -21,11 +23,29 @@ public class CreativeTabManager {
         }
     }
 
+    // グループ予約済みアイテムスタック
+    public static class BookingStack {
+        public ItemGroup itemGroup;
+        public ItemStack stack;
+        private BookingStack(ItemGroup itemGroup, ItemStack stack) {
+            this.itemGroup = itemGroup;
+            this.stack = stack;
+        }
+    }
+
     public static void allRegister() {
-        if (bookingItems.isEmpty()) return;
-        for (BookingItem bookingItem : bookingItems) {
-            CreativeTabRegistry.append(bookingItem.itemGroup, ItemUtil.fromId(bookingItem.identifier));
-            bookingItems.remove(bookingItem);
+        if (!bookingItems.isEmpty()) {
+            for (BookingItem bookingItem : bookingItems) {
+                CreativeTabRegistry.append(bookingItem.itemGroup, ItemUtil.fromId(bookingItem.identifier));
+                bookingItems.remove(bookingItem);
+            }
+        }
+
+        if (!bookingStacks.isEmpty()) {
+            for (BookingStack bookingStack : bookingStacks) {
+                CreativeTabRegistry.appendStack(bookingStack.itemGroup, bookingStack.stack);
+                bookingStacks.remove(bookingStack);
+            }
         }
     }
 
@@ -41,6 +61,9 @@ public class CreativeTabManager {
 
     public static void addItem(ItemGroup itemGroup, Identifier identifier) {
         bookingItems.add(new BookingItem(itemGroup, identifier));
+    }
 
+    public static void addStack(ItemGroup itemGroup, ItemStack stack) {
+        bookingStacks.add(new BookingStack(itemGroup, stack));
     }
 }
