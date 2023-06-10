a6cf1eaa28c45a7537d38f1348ae297ed36d2b7a
PTOM76 <ptms76@outlook.jp>
2023-06-10 23:44:46 +0900

    1.20

--- php/build.bat
+++ php/build.bat
@@ -1,6 +1,15 @@
 cd "..\"
 
 git fetch origin
+git checkout 1.20
+call gradlew.bat build
+call gradlew.bat generatePomFileForMavenCommonPublication
+call gradlew.bat generatePomFileForMavenForgePublication
+call gradlew.bat generatePomFileForMavenFabricPublication
+copy /y ".\common\build\publications\mavenCommon\pom-default.xml" ".\common\build\publications\mavenCommon\mcpitanlibarch-1.20-rc1.pom"
+copy /y ".\fabric\build\publications\mavenFabric\pom-default.xml" ".\fabric\build\publications\mavenFabric\mcpitanlibarch-1.20-rc1.pom"
+
+git fetch origin
 git checkout 1.19.4
 call gradlew.bat build
 call gradlew.bat generatePomFileForMavenCommonPublication
@@ -8,7 +17,7 @@ call gradlew.bat generatePomFileForMavenForgePublication
 call gradlew.bat generatePomFileForMavenFabricPublication
 copy /y ".\common\build\publications\mavenCommon\pom-default.xml" ".\common\build\publications\mavenCommon\mcpitanlibarch-1.19.4.pom"
 copy /y ".\fabric\build\publications\mavenFabric\pom-default.xml" ".\fabric\build\publications\mavenFabric\mcpitanlibarch-1.19.4.pom"
-copy /y ".\forge\build\publications\mavenForge\pom-default.xml" ".\forge\build\publications\mavenForge\mcpitanlibarch-1.19.4.pom"
+copy /y ".\forge\build\publications\mavenForge\pom-default.xml" ".\forge\build\publications\mavenForge\mcpitanlibarch-1.20.pom"
 
 git fetch origin
 git checkout 1.19.3
@@ -69,6 +78,4 @@ copy /y "C:\Users\ptms76\Documents\php\*.bat" ".\"
 copy /y "C:\Users\ptms76\Documents\php\*.php" ".\"
 
 start upload_maven.bat
-start upload_curseforge.bat
-start upload_modrinth.bat
 pause
