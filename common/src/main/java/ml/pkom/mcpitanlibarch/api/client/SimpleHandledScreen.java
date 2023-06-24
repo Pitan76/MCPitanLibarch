3785b4fa66199d4f8c0422877a36da9cf3802d37
Pitan <58260965+PTOM76@users.noreply.github.com>
2023-06-24 22:48:18 +0900

    add apis

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/client/SimpleHandledScreen.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/client/SimpleHandledScreen.java
@@ -1,10 +1,7 @@
 package ml.pkom.mcpitanlibarch.api.client;
 
 import ml.pkom.mcpitanlibarch.api.client.render.DrawObjectDM;
-import ml.pkom.mcpitanlibarch.api.client.render.handledscreen.DrawBackgroundArgs;
-import ml.pkom.mcpitanlibarch.api.client.render.handledscreen.DrawForegroundArgs;
-import ml.pkom.mcpitanlibarch.api.client.render.handledscreen.DrawMouseoverTooltipArgs;
-import ml.pkom.mcpitanlibarch.api.client.render.handledscreen.RenderArgs;
+import ml.pkom.mcpitanlibarch.api.client.render.handledscreen.*;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
@@ -36,6 +33,7 @@ public abstract class SimpleHandledScreen extends HandledScreen<ScreenHandler> {
         // addButton
     }
 
+    @Deprecated
     @Override
     protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
         DrawObjectDM drawObjectDM = new DrawObjectDM(context);
@@ -44,6 +42,7 @@ public abstract class SimpleHandledScreen extends HandledScreen<ScreenHandler> {
 
     public abstract void drawBackgroundOverride(DrawBackgroundArgs args);
 
+    @Deprecated
     @Override
     protected void drawForeground(DrawContext context, int mouseX, int mouseY) {
         DrawObjectDM drawObjectDM = new DrawObjectDM(context);
@@ -78,6 +77,7 @@ public abstract class SimpleHandledScreen extends HandledScreen<ScreenHandler> {
     public void initOverride() {
     }
 
+    @Deprecated
     @Override
     protected void init() {
         super.init();
@@ -85,6 +85,7 @@ public abstract class SimpleHandledScreen extends HandledScreen<ScreenHandler> {
         initOverride();
     }
 
+    @Deprecated
     @Override
     public void resize(MinecraftClient client, int width, int height) {
         super.resize(client, width, height);
@@ -150,11 +151,30 @@ public abstract class SimpleHandledScreen extends HandledScreen<ScreenHandler> {
         return super.backgroundHeight;
     }
 
+    @Deprecated
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         DrawObjectDM drawObjectDM = new DrawObjectDM(context);
         renderOverride(new RenderArgs(drawObjectDM, mouseX, mouseY, delta));
     }
 
+    public boolean keyReleased(KeyEventArgs args) {
+        return super.keyReleased(args.keyCode, args.scanCode, args.modifiers);
+    }
 
+    public boolean keyPressed(KeyEventArgs args) {
+        return super.keyPressed(args.keyCode, args.scanCode, args.modifiers);
+    }
+
+    @Deprecated
+    @Override
+    public boolean keyReleased(int keyCode, int scanCode, int modifiers) {
+        return this.keyReleased(new KeyEventArgs(keyCode, scanCode, modifiers));
+    }
+
+    @Deprecated
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        return this.keyPressed(new KeyEventArgs(keyCode, scanCode, modifiers));
+    }
 }
