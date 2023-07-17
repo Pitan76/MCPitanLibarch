6de014d99bbe86d82d82daad0c2920fb77e42cd4
Pitan <58260965+PTOM76@users.noreply.github.com>
2023-07-17 17:22:10 +0900

    Add APIs Fix APIs

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/util/DamageSourceUtil.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/util/DamageSourceUtil.java
@@ -6,35 +6,35 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 
 public class DamageSourceUtil {
-    public DamageSource thrownProjectile(Entity projectile, Entity attacker, Entity source) {
+    public static DamageSource thrownProjectile(Entity projectile, Entity attacker, Entity source) {
         return thrownProjectile(projectile, attacker);
     }
 
-    public DamageSource thrownProjectile(Entity projectile, Entity attacker) {
+    public static DamageSource thrownProjectile(Entity projectile, Entity attacker) {
         return DamageSource.thrownProjectile(projectile, attacker);
     }
 
-    public DamageSource playerAttack(Player attacker, Entity source) {
+    public static DamageSource playerAttack(Player attacker, Entity source) {
         return playerAttack(attacker);
     }
 
-    public DamageSource playerAttack(Player attacker) {
+    public static DamageSource playerAttack(Player attacker) {
         return DamageSource.player(attacker.getPlayerEntity());
     }
 
-    public DamageSource mobAttack(LivingEntity attacker, Entity source) {
+    public static DamageSource mobAttack(LivingEntity attacker, Entity source) {
         return mobAttack(attacker);
     }
 
-    public DamageSource mobAttack(LivingEntity attacker) {
+    public static DamageSource mobAttack(LivingEntity attacker) {
         return DamageSource.mob(attacker);
     }
 
-    public DamageSource mobProjectile(Entity projectile, LivingEntity attacker, Entity source) {
+    public static DamageSource mobProjectile(Entity projectile, LivingEntity attacker, Entity source) {
         return mobProjectile(projectile, attacker);
     }
 
-    public DamageSource mobProjectile(Entity projectile, LivingEntity attacker) {
+    public static DamageSource mobProjectile(Entity projectile, LivingEntity attacker) {
         return DamageSource.mobProjectile(projectile, attacker);
     }
 }
