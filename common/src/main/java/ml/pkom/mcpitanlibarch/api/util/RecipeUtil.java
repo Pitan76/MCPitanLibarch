68205048df0021779a507271c653dbdb4f2fff81
Pitan <58260965+PTOM76@users.noreply.github.com>
2023-11-07 22:18:34 +0900

    add APIs

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/util/RecipeUtil.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/util/RecipeUtil.java
@@ -4,10 +4,15 @@ import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeEntry;
 import net.minecraft.recipe.ShapelessRecipe;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class RecipeUtil {
     public static ShapelessRecipe createShapelessRecipe(Identifier id, String group, CompatibilityCraftingRecipeCategory category, ItemStack output, DefaultedList<Ingredient> input) {
@@ -22,9 +27,18 @@ public class RecipeUtil {
         return recipe.craft(inventory);
     }
 
-
     public static <C extends Inventory> ItemStack getOutput(Recipe<C> recipe, World world) {
         return recipe.getOutput();
+    }
+
+    public static List<Recipe<?>> getAllRecipes(World world) {
+        Collection<RecipeEntry<?>> recipes = world.getRecipeManager().values();
+        List<Recipe<?>> outRecipes = new ArrayList<>();
+        for (RecipeEntry<?> recipeEntry : recipes) {
+            Recipe<?> recipe = recipeEntry.value();
+            outRecipes.add(recipe);
+        }
+        return outRecipes;
     }
 
     public enum CompatibilityCraftingRecipeCategory {
