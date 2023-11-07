d0e17709430dfe36e31e8d4bfb25b2456af01dd6
Pitan <58260965+PTOM76@users.noreply.github.com>
2023-11-07 22:23:55 +0900

    add APIs-2

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/util/RecipeUtil.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/util/RecipeUtil.java
@@ -4,7 +4,6 @@ import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.Recipe;
-import net.minecraft.recipe.RecipeEntry;
 import net.minecraft.recipe.ShapelessRecipe;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
@@ -32,13 +31,8 @@ public class RecipeUtil {
     }
 
     public static List<Recipe<?>> getAllRecipes(World world) {
-        Collection<RecipeEntry<?>> recipes = world.getRecipeManager().values();
-        List<Recipe<?>> outRecipes = new ArrayList<>();
-        for (RecipeEntry<?> recipeEntry : recipes) {
-            Recipe<?> recipe = recipeEntry.value();
-            outRecipes.add(recipe);
-        }
-        return outRecipes;
+        Collection<Recipe<?>> recipes = world.getRecipeManager().values();
+        return recipes.stream().toList();
     }
 
     public enum CompatibilityCraftingRecipeCategory {
--- common/src/main/java/ml/pkom/mcpitanlibarch/mixin/ItemStackMixin.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/mixin/ItemStackMixin.java
@@ -3,6 +3,7 @@ package ml.pkom.mcpitanlibarch.mixin;
 import ml.pkom.mcpitanlibarch.api.event.v0.event.ItemStackActionEvent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class ItemStackMixin {
 
     @Inject(method = "damage(ILnet/minecraft/util/math/random/Random;Lnet/minecraft/server/network/ServerPlayerEntity;)Z", at = @At("HEAD"), cancellable = true)
-    public void damage(int amount, net.minecraft.util.math.random.Random random, ServerPlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
+    public void damage(int amount, Random random, ServerPlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
         ItemStackActionEvent.call((ItemStack) (Object) this);
     }
 }
