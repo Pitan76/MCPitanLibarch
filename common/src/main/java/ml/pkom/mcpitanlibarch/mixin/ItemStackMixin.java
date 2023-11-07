2101d06af2d1365de9027329e3a66e92bb537fb1
Pitan <58260965+PTOM76@users.noreply.github.com>
2023-11-08 00:02:42 +0900

    add APIs

--- common/src/main/java/ml/pkom/mcpitanlibarch/mixin/ItemStackMixin.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/mixin/ItemStackMixin.java
@@ -15,5 +15,7 @@ public abstract class ItemStackMixin {
     @Inject(method = "damage(ILnet/minecraft/util/math/random/Random;Lnet/minecraft/server/network/ServerPlayerEntity;)Z", at = @At("HEAD"), cancellable = true)
     public void damage(int amount, Random random, ServerPlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
         ItemStackActionEvent.call((ItemStack) (Object) this);
+        if (ItemStackActionEvent.returnValue != null)
+            cir.setReturnValue(ItemStackActionEvent.returnValue);
     }
 }
