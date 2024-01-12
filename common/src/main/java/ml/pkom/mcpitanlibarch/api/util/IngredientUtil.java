3254746cdaa4015a065744205876ac54af19faef
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-01-12 22:25:23 +0900

    Fix IngredientUtil 1192

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/util/IngredientUtil.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/util/IngredientUtil.java
@@ -1,11 +1,13 @@
 package ml.pkom.mcpitanlibarch.api.util;
 
 import net.minecraft.recipe.Ingredient;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class IngredientUtil {
     public static Ingredient fromTagByIdentifier(Identifier id) {
-        return Ingredient.fromTag(TagKey.of(Registries.ITEM.getKey(), id));
+        return Ingredient.fromTag(TagKey.of(Registry.ITEM_KEY, id));
     }
 
     public static Ingredient fromTagByString(String id) {
