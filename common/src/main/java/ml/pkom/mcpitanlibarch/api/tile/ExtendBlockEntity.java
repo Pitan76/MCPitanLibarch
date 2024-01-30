78c387b52a1bfe401a38b96cac467e80318ed360
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-01-30 16:25:22 +0900

    add APIs

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/tile/ExtendBlockEntity.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/tile/ExtendBlockEntity.java
@@ -3,6 +3,7 @@ package ml.pkom.mcpitanlibarch.api.tile;
 import ml.pkom.mcpitanlibarch.api.block.ExtendBlockEntityProvider;
 import ml.pkom.mcpitanlibarch.api.event.block.TileCreateEvent;
 import ml.pkom.mcpitanlibarch.api.event.tile.TileTickEvent;
+import ml.pkom.mcpitanlibarch.api.packet.UpdatePacketType;
 import ml.pkom.mcpitanlibarch.api.world.ExtendWorld;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -10,8 +11,12 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Tickable;
+import net.minecraft.network.listener.ClientPlayPacketListener;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 public class ExtendBlockEntity extends BlockEntity implements Tickable {
     ExtendWorld world;
@@ -26,6 +31,21 @@ public class ExtendBlockEntity extends BlockEntity implements Tickable {
     public void setWorld(World world) {
         super.setLocation(world, pos);
         this.world = new ExtendWorld(world);
+    }
+
+    @Nullable
+    @Override
+    @Deprecated
+    public Packet<ClientPlayPacketListener> toUpdatePacket() {
+        switch (getUpdatePacketType().name) {
+            case "BLOCK_ENTITY_UPDATE_S2C":
+                return BlockEntityUpdateS2CPacket.create(this);
+        }
+        return super.toUpdatePacket();
+    }
+
+    public UpdatePacketType getUpdatePacketType() {
+        return UpdatePacketType.NONE;
     }
 
     public void writeNbtOverride(NbtCompound nbt) {
---
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/util/ActionResultUtil.java
@@ -0,0 +1,30 @@
+package ml.pkom.mcpitanlibarch.api.util;
+
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.TypedActionResult;
+
+public class ActionResultUtil {
+
+    public static <T> TypedActionResult<T> typedActionResult(ActionResult result, T t, boolean swingHand) {
+        switch (result) {
+            case PASS:
+                return TypedActionResult.pass(t);
+            case SUCCESS:
+                return TypedActionResult.success(t, swingHand);
+            case FAIL:
+                return TypedActionResult.fail(t);
+            case CONSUME:
+            case CONSUME_PARTIAL:
+                return TypedActionResult.consume(t);
+        }
+        return TypedActionResult.pass(t);
+    }
+
+    public static <T> TypedActionResult<T> typedActionResult(ActionResult result, T t) {
+        return typedActionResult(result, t, true);
+    }
+
+    public static <T> ActionResult actionResult(TypedActionResult<T> result) {
+        return result.getResult();
+    }
+}
