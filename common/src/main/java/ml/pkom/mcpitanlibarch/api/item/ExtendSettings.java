751e92afe4f88e30d6187fc3d302c922969b2bc5
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-05 14:18:53 +0900

    fix apis, add apis

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/item/ExtendSettings.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/item/ExtendSettings.java
@@ -4,19 +4,26 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Supplier;
+
 public class ExtendSettings extends Item.Settings {
 
     // ～1.19.2
+    @Deprecated
     public ExtendSettings addGroup(ItemGroup itemGroup) {
         return this;
     }
 
     // 1.19.3～
     // identifier: Item ID
+    @Deprecated
     public ExtendSettings addGroup(ItemGroup itemGroup, Identifier identifier) {
         CreativeTabManager.addItem(itemGroup, identifier);
         return this;
     }
 
-
+    public ExtendSettings addGroup(Supplier<ItemGroup> itemGroup, Identifier identifier) {
+        CreativeTabManager.addItem(itemGroup, identifier);
+        return this;
+    }
 }
