693f7a500c9b92bca5bc2fe660c142ca61c39c4c
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-02-05 14:18:53 +0900

    fix apis, add apis

---
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/command/ConfigCommand.java
@@ -0,0 +1,151 @@
+package ml.pkom.mcpitanlibarch.api.command;
+
+import com.mojang.brigadier.arguments.StringArgumentType;
+import ml.pkom.easyapi.config.Config;
+import ml.pkom.mcpitanlibarch.api.command.argument.StringCommand;
+import ml.pkom.mcpitanlibarch.api.event.ServerCommandEvent;
+import ml.pkom.mcpitanlibarch.api.event.StringCommandEvent;
+import ml.pkom.mcpitanlibarch.api.util.TextUtil;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.util.function.Supplier;
+
+public class ConfigCommand extends LiteralCommand {
+
+    public final Config config;
+    public File file;
+    public Supplier<Void> defaultConfigFunction;
+    public String prefix = "[MCPitanLib]";
+    
+    public ConfigCommand(Config config, File file, String prefix) {
+        this.config = config;
+        this.file = file;
+        this.prefix = prefix;
+    }
+    
+    public ConfigCommand(Config config, File file, String prefix, @Nullable Supplier<Void> supplier) {
+        this.config = config;
+        this.file = file;
+        this.defaultConfigFunction = supplier;
+        this.prefix = prefix;
+    }
+    
+    public ConfigCommand(Config config, File file, @Nullable Supplier<Void> supplier) {
+        this.config = config;
+        this.file = file;
+        this.defaultConfigFunction = supplier;
+    }
+
+    public ConfigCommand(Config config, File file) {
+        this.config = config;
+        this.file = file;
+    }
+
+    public ConfigCommand(Config config) {
+        this.config = config;
+    }
+
+    @Override
+    public void init(CommandSettings settings) {
+        settings.permissionLevel(3);
+
+        addArgumentCommand("set", new StringCommand() {
+            @Override
+            public String getArgumentName() {
+                return "key";
+            }
+
+            @Override
+            public void init(CommandSettings settings) {
+
+                addArgumentCommand("value", new StringCommand() {
+                    @Override
+                    public String getArgumentName() {
+                        return "value";
+                    }
+
+                    @Override
+                    public void execute(StringCommandEvent event) {
+                        String key = StringArgumentType.getString(event.context, "key");
+                        String value = StringArgumentType.getString(event.context, "value");
+                        if (config.get(key) == null) {
+                            event.sendFailure(TextUtil.literal(prefix + " Key not found."));
+                            return;
+                        }
+                        if (config.get(key).getClass() == String.class) {
+                            config.setString(key, value);
+                            event.sendSuccess(TextUtil.literal(prefix + " Set " + key + " to " + value), false);
+
+                        } else if (config.get(key).getClass() == Integer.class) {
+                            config.setInt(key, Integer.parseInt(value));
+                            event.sendSuccess(TextUtil.literal(prefix + " Set " + key + " to " + value), false);
+
+                        } else if (config.get(key).getClass() == Double.class) {
+                            config.setDouble(key, Double.parseDouble(value));
+                            event.sendSuccess(TextUtil.literal(prefix + " Set " + key + " to " + value), false);
+
+                        } else if (config.get(key).getClass() == Boolean.class) {
+                            config.setBoolean(key, Boolean.parseBoolean(value));
+                            event.sendSuccess(TextUtil.literal(prefix + " Set " + key + " to " + value), false);
+
+                        } else {
+                            event.sendFailure(TextUtil.literal(prefix + " Not supported type."));
+                        }
+                        if (file != null)
+                            config.save(file);
+                    }
+                });
+            }
+
+            @Override
+            public void execute(StringCommandEvent event) {
+            }
+        });
+
+        addArgumentCommand("get", new StringCommand() {
+            @Override
+            public String getArgumentName() {
+                return "key";
+            }
+
+            @Override
+            public void execute(StringCommandEvent event) {
+                String key = StringArgumentType.getString(event.context, "key");
+                if (config.get(key) == null) {
+                    event.sendFailure(TextUtil.literal(prefix + " Key not found."));
+                    return;
+                }
+                event.sendSuccess(TextUtil.literal(prefix + " " + key + ": " + config.get(key).toString()), false);
+            }
+        });
+
+        addArgumentCommand("list", new LiteralCommand() {
+            @Override
+            public void execute(ServerCommandEvent event) {
+                event.sendSuccess(TextUtil.literal(prefix + " Config List"), false);
+                for (String key : config.configMap.keySet()) {
+                    event.sendSuccess(TextUtil.literal(" - " + key + ": " + config.get(key).toString()), false);
+                }
+            }
+        });
+
+        if (defaultConfigFunction != null) {
+        addArgumentCommand("reset", new LiteralCommand() {
+            @Override
+            public void execute(ServerCommandEvent event) {
+                config.configMap.clear();
+                defaultConfigFunction.get();
+                if (file != null)
+                    config.save(file);
+                event.sendSuccess(TextUtil.literal(prefix + " Reset config."), false);
+            }
+        });
+        }
+    }
+
+    @Override
+    public void execute(ServerCommandEvent event) {
+
+    }
+}
--- common/src/main/java/ml/pkom/mcpitanlibarch/api/item/ExtendSettings.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/item/ExtendSettings.java
@@ -4,9 +4,12 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Supplier;
+
 public class ExtendSettings extends Item.Settings {
 
     // ～1.19.2
+    @Deprecated
     public ExtendSettings addGroup(ItemGroup itemGroup) {
         super.group(itemGroup);
         return this;
@@ -14,8 +17,14 @@ public class ExtendSettings extends Item.Settings {
 
     // 1.19.3～
     // identifier: Item ID
+    @Deprecated
     public ExtendSettings addGroup(ItemGroup itemGroup, Identifier identifier) {
         //CreativeTabRegistry.append(itemGroup, ItemUtil.fromId(identifier));
         return addGroup(itemGroup);
+    }
+
+    public ExtendSettings addGroup(Supplier<ItemGroup> itemGroup, Identifier identifier) {
+        addGroup(itemGroup.get());
+        return this;
     }
 }
