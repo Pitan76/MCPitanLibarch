bd4834dcb863bf17dc6b5b42171e3cacc1a5581d
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-05 20:52:08 +0900

    add apis

---
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/item/tool/CompatibleAxeItem.java
@@ -0,0 +1,34 @@
+package ml.pkom.mcpitanlibarch.api.item.tool;
+
+import ml.pkom.mcpitanlibarch.api.item.CompatibleItemSettings;
+import ml.pkom.mcpitanlibarch.api.item.ExtendItemProvider;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.PickaxeItem;
+
+public class CompatibleAxeItem extends AxeItem implements ExtendItemProvider {
+    public CompatibleAxeItem(CompatibleToolMaterial material, float attackDamage, float attackSpeed, CompatibleItemSettings settings) {
+        super(material, attackDamage, attackSpeed, settings.build());
+    }
+
+    public boolean overrideIsSuitableFor(BlockState state) {
+        return super.isSuitableFor(state);
+    }
+
+    @Deprecated
+    @Override
+    public boolean isSuitableFor(BlockState state) {
+        return overrideIsSuitableFor(state);
+    }
+
+    public float overrideGetMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+        return super.getMiningSpeedMultiplier(stack, state);
+    }
+
+    @Deprecated
+    @Override
+    public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+        return overrideGetMiningSpeedMultiplier(stack, state);
+    }
+}
