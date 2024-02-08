34bdb2441ab862beda2c49263063e11576867055
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-02-08 17:18:47 +0900

    add APIs

--- common/src/main/java/ml/pkom/mcpitanlibarch/api/event/item/CraftEvent.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/api/event/item/CraftEvent.java
@@ -1,19 +1,16 @@
 package ml.pkom.mcpitanlibarch.api.event.item;
 
-import ml.pkom.mcpitanlibarch.api.event.BaseEvent;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
-public class ItemFinishUsingEvent extends BaseEvent {
+public class CraftEvent {
+
     public ItemStack stack;
     public World world;
-    public LivingEntity user;
 
-    public ItemFinishUsingEvent(ItemStack stack, World world, LivingEntity user) {
+    public CraftEvent(ItemStack stack, World world) {
         this.stack = stack;
         this.world = world;
-        this.user = user;
     }
 
     public ItemStack getStack() {
@@ -22,10 +19,6 @@ public class ItemFinishUsingEvent extends BaseEvent {
 
     public World getWorld() {
         return world;
-    }
-
-    public LivingEntity getUser() {
-        return user;
     }
 
     public boolean isClient() {
