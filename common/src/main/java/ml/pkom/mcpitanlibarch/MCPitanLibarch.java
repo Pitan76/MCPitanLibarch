99c001c419b724e6e40534a4cce1a1ea4d9577ab
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-02-09 10:58:23 +0900

    fix APIs add Debug mode

--- common/src/main/java/ml/pkom/mcpitanlibarch/MCPitanLibarch.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/MCPitanLibarch.java
@@ -2,7 +2,10 @@ package ml.pkom.mcpitanlibarch;
 
 import ml.pkom.easyapi.config.Config;
 import ml.pkom.easyapi.config.JsonConfig;
+import ml.pkom.mcpitanlibarch.api.registry.ArchRegistry;
 import ml.pkom.mcpitanlibarch.api.util.PlatformUtil;
+import ml.pkom.mcpitanlibarch.debug.DebugTool;
+import net.minecraft.util.Identifier;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -20,15 +23,28 @@ public class MCPitanLibarch {
     public static List<String> itemBlackList = new ArrayList<>();
     public static List<String> blockBlackList = new ArrayList<>();
 
+    // MCPitanLibのアイテム、ブロック、タイルエンティティの登録
+    public static ArchRegistry registry = ArchRegistry.createRegistry(MOD_ID);
+
+    /**
+     * MCPitanLibの初期化
+     */
     public static void init() {
         configInit();
-        /*
-        if (PlatformUtil.isDevelopmentEnvironment()) {
+
+        if (PlatformUtil.isDevelopmentEnvironment() || (config.has("debugMode") && config.getBoolean("debugMode"))) {
+            System.out.println("MCPitanLib: Debug Mode");
+            DebugTool.register();
+            registry.allRegister();
+            /*
             ExampleMod.init();
+            */
         }
-         */
     }
 
+    /**
+     * Configの初期化
+     */
     public static void configInit() {
         try {
             if (configLoaded) return;
@@ -39,14 +55,18 @@ public class MCPitanLibarch {
 
             config.setString("item", "examplemod:hogehoge_item,examplemod:fuga_item");
             config.setString("block", "examplemod:hogehoge_block,examplemod:fuga_block");
+            config.setBoolean("debugMode", false);
 
             if (configFile.exists())
                 config.load(configFile);
 
-            if (config.configMap.containsKey("item"))
+            if (!config.has("debugMode"))
+                config.setBoolean("debugMode", false);
+
+            if (config.has("item"))
                 itemBlackList.addAll(Arrays.asList(config.getString("item").split(",")));
 
-            if (config.configMap.containsKey("block"))
+            if (config.has("block"))
                 blockBlackList.addAll(Arrays.asList(config.getString("block").split(",")));
 
             config.save(configFile);
@@ -55,4 +75,12 @@ public class MCPitanLibarch {
         }
     }
 
+    /**
+     * Identifierを生成
+     * @param path パス
+     * @return Identifier
+     */
+    public static Identifier id(String path) {
+        return new Identifier(MOD_ID, path);
+    }
 }
