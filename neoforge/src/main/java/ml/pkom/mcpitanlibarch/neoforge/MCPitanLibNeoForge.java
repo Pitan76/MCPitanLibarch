172e6774a74f27d21b34c20dd9cbe9278337864d
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-13 02:02:50 +0900

    neoforge

---
+++ neoforge/src/main/java/ml/pkom/mcpitanlibarch/neoforge/MCPitanLibNeoForge.java
@@ -0,0 +1,15 @@
+package ml.pkom.mcpitanlibarch.neoforge;
+
+import net.neoforged.fml.common.Mod;
+import net.pitan76.mcpitanlib.MCPitanLib;
+
+@Mod(MCPitanLib.MOD_ID)
+public class MCPitanLibNeoForge {
+    public MCPitanLibNeoForge() {
+        //EventBuses.registerModEventBus(MCPitanLibarch.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        MCPitanLib.init();
+
+        //if (Platform.getEnv().isClient())
+        //    FMLJavaModLoadingContext.get().getModEventBus().addListener(MCPitanLibarchNeoForgeClient::clientInit);
+    }
+}
