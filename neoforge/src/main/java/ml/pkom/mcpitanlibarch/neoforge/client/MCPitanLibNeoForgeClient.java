e1c14eda52ae989ce5c20b045cbc2bfecdd70604
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-13 02:02:50 +0900

    neoforge

--- neoforge/src/main/java/ml/pkom/mcpitanlibarch/neoforge/client/MCPitanLibNeoForgeClient.java
+++ neoforge/src/main/java/ml/pkom/mcpitanlibarch/neoforge/client/MCPitanLibNeoForgeClient.java
@@ -2,7 +2,7 @@ package ml.pkom.mcpitanlibarch.neoforge.client;
 
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
-public class MCPitanLibarchNeoForgeClient {
+public class MCPitanLibNeoForgeClient {
     public static void clientInit(FMLClientSetupEvent event) {
         //if (PlatformUtil.isDevelopmentEnvironment())
         //    ExampleModClient.init();
