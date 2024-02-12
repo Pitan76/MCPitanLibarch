50985e21336925694e4c2865b807c68bbd88704c
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-02-12 23:05:53 +0900

    v1.9.3 add APIs, fix APIs embed cloth config api

--- common/src/main/java/ml/pkom/mcpitanlibarch/test/ExampleMod.java
+++ common/src/main/java/ml/pkom/mcpitanlibarch/test/ExampleMod.java
@@ -1,14 +1,16 @@
 package ml.pkom.mcpitanlibarch.test;
 
-import dev.architectury.registry.menu.MenuRegistry;
+import ml.pkom.mcpitanlibarch.api.block.CompatibleBlockSettings;
+import ml.pkom.mcpitanlibarch.api.block.CompatibleMaterial;
+import ml.pkom.mcpitanlibarch.api.block.ExtendBlock;
 import ml.pkom.mcpitanlibarch.api.command.CommandRegistry;
-import ml.pkom.mcpitanlibarch.api.event.registry.RegistryEvent;
+import ml.pkom.mcpitanlibarch.api.event.registry.RegistryResult;
+import ml.pkom.mcpitanlibarch.api.gui.SimpleScreenHandlerTypeBuilder;
 import ml.pkom.mcpitanlibarch.api.item.DefaultItemGroups;
 import ml.pkom.mcpitanlibarch.api.item.ExtendSettings;
 import ml.pkom.mcpitanlibarch.api.registry.ArchRegistry;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.screen.ScreenHandlerType;
@@ -32,25 +34,25 @@ public class ExampleMod {
 
     public static ArchRegistry registry = ArchRegistry.createRegistry(MOD_ID);
 
-    public static RegistryEvent<ScreenHandlerType<?>> supplierEXAMPLE_SCREENHANDLER = registry.registerScreenHandlerType(id("example_gui"), () -> MenuRegistry.of(ExampleScreenHandler::new));
+    public static RegistryResult<ScreenHandlerType<?>> supplierEXAMPLE_SCREENHANDLER = registry.registerScreenHandlerType(id("example_gui"), () -> new SimpleScreenHandlerTypeBuilder<>(ExampleScreenHandler::new).build());
 
-    public static RegistryEvent<Item> EXAMPLE_ITEM_SUPPLIER;
-    public static RegistryEvent<Block> EXAMPLE_BLOCK_SUPPLIER;
-    public static RegistryEvent<Item> EXAMPLE_BLOCK_ITEM_SUPPLIER;
-    public static RegistryEvent<Item> EXAMPLE_GUI_ITEM_SUPPLIER;
-    public static RegistryEvent<Block> EXAMPLE_GUI_BLOCK_SUPPLIER;
-    public static RegistryEvent<Item> EXAMPLE_GUI_BLOCK_ITEM_SUPPLIER;
+    public static RegistryResult<Item> EXAMPLE_ITEM_SUPPLIER;
+    public static RegistryResult<Block> EXAMPLE_BLOCK_SUPPLIER;
+    public static RegistryResult<Item> EXAMPLE_BLOCK_ITEM_SUPPLIER;
+    public static RegistryResult<Item> EXAMPLE_GUI_ITEM_SUPPLIER;
+    public static RegistryResult<Block> EXAMPLE_GUI_BLOCK_SUPPLIER;
+    public static RegistryResult<Item> EXAMPLE_GUI_BLOCK_ITEM_SUPPLIER;
 
     public static void init() {
         //EntityTypeBuilder.create().setSpawnGroup(SpawnGroup.AMBIENT).setEntityFactory(((type, world) -> new ZombieEntity((EntityType<? extends ZombieEntity>) type, world)));
 
         EXAMPLE_ITEM_SUPPLIER = registry.registerItem(id("example_item"), () -> new Item(new ExtendSettings().addGroup(DefaultItemGroups.INGREDIENTS, id("example_item"))));
-        EXAMPLE_BLOCK_SUPPLIER = registry.registerBlock(id("example_block"), () -> new Block(AbstractBlock.Settings.of(Material.STONE)));
+        EXAMPLE_BLOCK_SUPPLIER = registry.registerBlock(id("example_block"), () -> new ExtendBlock(CompatibleBlockSettings.of(CompatibleMaterial.STONE)));
         EXAMPLE_BLOCK_ITEM_SUPPLIER = registry.registerItem(id("example_block"), () -> new BlockItem(EXAMPLE_BLOCK_SUPPLIER.supplier.get(), new ExtendSettings().addGroup(DefaultItemGroups.INGREDIENTS)));
 
         EXAMPLE_GUI_ITEM_SUPPLIER = registry.registerItem(id("example_gui_item"), () -> new ExampleGuiItem(new ExtendSettings().addGroup(DefaultItemGroups.INGREDIENTS)));
 
-        EXAMPLE_GUI_BLOCK_SUPPLIER = registry.registerBlock(id("example_gui_block"), () -> new ExampleGuiBlock(AbstractBlock.Settings.of(Material.STONE)));
+        EXAMPLE_GUI_BLOCK_SUPPLIER = registry.registerBlock(id("example_gui_block"), () -> new ExampleGuiBlock(CompatibleBlockSettings.of(CompatibleMaterial.STONE).build()));
         EXAMPLE_GUI_BLOCK_ITEM_SUPPLIER = registry.registerItem(id("example_gui_block"), () -> new BlockItem(EXAMPLE_GUI_BLOCK_SUPPLIER.supplier.get(), new ExtendSettings().addGroup(DefaultItemGroups.INGREDIENTS)));
 
         registry.allRegister();
