dc19ef604d00bb7ffb05664644215b19ef37c81c
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-02-14 21:29:49 +0900

    add ItemUtil API

--- common/src/main/java/net/pitan76/mcpitanlib/api/util/v1/ItemUtilV1.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/v1/ItemUtilV1.java
@@ -1,4 +1,4 @@
-package ml.pkom.mcpitanlibarch.api.util;
+package net.pitan76.mcpitanlib.api.util.v1;
 
 import ml.pkom.mcpitanlibarch.api.item.CompatibleItemSettings;
 import ml.pkom.mcpitanlibarch.api.tag.TagKey;
@@ -6,72 +6,76 @@ import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Deprecated
-public class ItemUtil {
-    private static net.pitan76.mcpitanlib.api.util.ItemUtil newItemUtil = new net.pitan76.mcpitanlib.api.util.ItemUtil();
-
+public class ItemUtilV1 {
     public static Item item(Identifier id) {
-        return newItemUtil.item(id);
+        return Registries.ITEM.get(id);
     }
 
     public static boolean isEqual(Item item, Item item2) {
-        return newItemUtil.isEqual(item, item2);
+        return item == item2;
     }
 
     public static boolean isOf(ItemStack stack, Item item) {
-        return newItemUtil.isOf(stack, item);
+        return stack.isOf(item);
     }
 
     public static boolean isIn(ItemStack stack, TagKey<Item> tagKey) {
-        return newItemUtil.isIn(stack, tagKey);
+        return isIn(stack.getItem(), tagKey);
     }
 
     public static boolean isIn(Item item, TagKey<Item> tagKey) {
-        return newItemUtil.isIn(item, tagKey);
+        if (item.getRegistryEntry().isIn(tagKey.getTagKey())) return true;
+        return tagKey.isOf(item);
     }
 
     public static boolean isExist(Identifier identifier) {
-        return newItemUtil.isExist(identifier);
+        return Registries.ITEM.containsId(identifier);
     }
     public static Identifier toID(Item item) {
-        return newItemUtil.toID(item);
+        return Registries.ITEM.getId(item);
     }
 
     public static Item fromId(Identifier identifier) {
-        return newItemUtil.fromId(identifier);
+        return Registries.ITEM.get(identifier);
     }
 
     @Deprecated
     public static BlockItem ofBlock(Block block, Item.Settings settings) {
-        return newItemUtil.ofBlock(block, settings);
+        return new BlockItem(block, settings);
     }
 
     public static BlockItem ofBlock(Block block, CompatibleItemSettings settings) {
-        return newItemUtil.ofBlock(block, settings);
+        return ofBlock(block, settings.build());
     }
 
     @Deprecated
     public static Item of(Item.Settings settings) {
-        return newItemUtil.of(settings);
+        return new Item(settings);
     }
 
     public static Item of(CompatibleItemSettings settings) {
-        return newItemUtil.of(settings);
+        return of(settings.build());
     }
 
     public static List<Item> getAllItems() {
-        return newItemUtil.getAllItems();
+        List<Item> items = new ArrayList<>();
+        for (Item item : Registries.ITEM) {
+            items.add(item);
+        }
+        return items;
     }
 
     public static int getRawId(Item item) {
-        return newItemUtil.getRawId(item);
+        return Registries.ITEM.getRawId(item);
     }
 
     public static Item fromIndex(int index) {
-        return newItemUtil.fromIndex(index);
+        return Registries.ITEM.get(index);
     }
 }
